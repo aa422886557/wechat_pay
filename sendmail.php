e54865aaa4afc2fc837c4a96e4e529463e91bed5
aa422886557 <422886557@qq.com>
2016-09-07 10:38:37 +0800

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -8,7 +8,7 @@ $smtpemailto = "422886557@qq.com";//发送给谁
 $smtpuser = "aa422886551@163.com";//SMTP服务器的用户帐号 
 $smtppass = "test001";//SMTP服务器的用户密码 
 $mailsubject = "PHP100测试邮件系统";//邮件主题 
-$mailbody = "<h1> 这是一个测试程序 PHP100.com </h1>";//邮件内容 
+$mailbody = "<h1> 这是一个测试程序 bugscan.vip </h1>";//邮件内容 
 $mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件 
 ########################################## 
 $smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证. 
